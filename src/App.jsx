654976a5a73cdfb84857d57ead94f02215f4f40e
Thiez-64 <ma.ojeanson@gmail.com>
2021-04-09 09:31:50 +0200

    silder file has been created

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
-
-import logo from './logo.png';
+import Slider from './components/slider'
 import './App.css';
 
 function App() {
   return (
-    <main className="rsw-container">
-      <div
-        className="rsw-item"
-        style={{
-          paddingBottom: '2rem',
-        }}>
-        <img src={logo} width="20%" alt="WCS logo" />
-      </div>
-      <div className="rsw-item">
-        <p>Welcome to your fresh, lightweight, React App ! &#127752;</p>
-      </div>
-      <div className="rsw-item">
-        <p>
-          Start in the <code>App.jsx</code> component !
-        </p>
-      </div>
-    </main>
+    <div className="app">
+      <Slider />
+    </div>
   );
 }
 
