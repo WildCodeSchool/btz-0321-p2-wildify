f475208b693c2c880b71aaa7128ba0dd2950f440
Thomas <barrialthomas@gmail.com>
2021-04-11 19:33:10 +0200

    MadeStructure CSS/AppCSS Responsive and Desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import './App.css'
+import Header from './components/header/header.jsx'
+import SideBar from './components/sideBar/sideBar'
 import Playbar from '../src/components/playbar/Playbar'
 import OnListen from './feature/apicall/OnListen'
 
@@ -11,9 +13,51 @@ function App() {
  
   return (
   
-    <div>
-      <Playbar/>
-    </div>
+ 
+
+        <div className="AppBody">
+              
+       
+
+      <div className="GridComponent">
+
+          <Header />
+
+            <div className="section1">
+            {/* The Main Component GoHere */} 
+            </div>
+
+        <div className="section2">
+
+             
+            <div className="PlaylistComponent">{/* PlaylistComponent GoHere */} </div>
+
+              <div className="section2Left">
+                
+                <div className="vignettes">
+                  <div className="vignette1">{/* ArtistComponent GoHere */} </div>
+                  <div className="vignette2">{/* AlbumComponent GoHere */} </div>
+                </div>
+
+                <div className="MixtapesComponent">{/* MixtapesComponent GoHere */}</div>
+
+              </div>
+
+        </div>
+
+        <div className="ContactForm">{/* ContactFormComponent GoHere */} </div>
+      
+      </div>
+          
+          <SideBar />
+          
+        </div> 
+        
+        
+       
+
+
+
   )
 }
 
