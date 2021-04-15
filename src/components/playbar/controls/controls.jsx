e280083c5432d72702d31dc184132ccea4790dfc
JIdayyy <julienabbadie1@gmail.com>
2021-04-15 11:16:37 +0200

    commit before merge

--- src/components/playbar/controls/controls.jsx
+++ src/components/playbar/controls/controls.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './controls.css';
 export default function Controls({ handlePause, handlePlay, handleBackWard, handleForWard }) {
   return (
     <div className="flex mx-6 w-6/12">
-      <button onClick={handleBackWard}>
+      <button className="outline-none" onClick={handleBackWard}>
         <img
           className="px-1 hover:opacity-50 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 ..."
           src="./src/img/backward.svg "
           alt=""
         />
       </button>
-      <button onClick={handlePlay}>
+      <button className="outline-none" onClick={handlePlay}>
         <img
           className="px-1 hover:opacity-50 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 ..."
           src="./src/img/play.svg"
           alt=""
         />
       </button>
-      <button onClick={handlePause}>
+      <button className="outline-none" onClick={handlePause}>
         <img
           className="px-1 hover:opacity-50 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 ..."
           src="./src/img/pause.png"
           alt=""
         />
       </button>
-      <button onClick={handleForWard}>
+      <button className="outline-none" onClick={handleForWard}>
         <img
           className="px-1 hover:opacity-50 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 ..."
           src="./src/img/forward.svg"
