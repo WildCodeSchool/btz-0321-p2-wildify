e87b4509a9714ca3e3cc3e53c9104e967abef254
JIdayyy <julienabbadie1@gmail.com>
2021-04-30 00:38:45 +0200

    app useeffect fix and upload progress bar ok

--- src/components/AdminPannel/AdminSongs/UploadSongs/UploadSongs.jsx
+++ src/components/AdminPannel/AdminSongs/UploadSongs/UploadSongs.jsx
@@ -1,50 +1,61 @@
 import React, { useState, useContext } from 'react';
 import authContext from '../../../../context/authContext';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 export default function UploadSongs({ playList, albums }) {
   const [albumId, setAlbumId] = useState();
   const [imgUrl, setImgUrl] = useState();
   const [selectFile, setSelectFile] = useState();
   const { token } = useContext(authContext);
-
+  const [progress, setProgress] = useState(0);
   const handleFile = (e) => {
     setSelectFile(e.target.files[0]);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = () => {
     const formData = new FormData();
-
     formData.append('file', selectFile);
-    fetch('https://bazify-backend.basile.vernouillet.dev/api/v1/songs', {
-      headers: { Authorization: `Bearer ${token}` },
-      method: 'POST',
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
+    // axios({
+    //   method: 'POST',
+    //   headers: { Authorization: `Bearer ${token}` },
+    //   url: 'https://bazify-backend.basile.vernouillet.dev/api/v1/songs',
+    //   data: formData,
+    //   onUploadProgress: (p) => {
+    //     setProgress((p.loaded / p.total) * 100);
+    //   },
+    // }).then((res) => console.log(res)),
+    //   (error) => {
+    //     console.log(error);
+    //   };
+    axios
+      .post('https://bazify-backend.basile.vernouillet.dev/api/v1/songs', formData, {
+        headers: { Authorization: `Bearer ${token}` },
+        onUploadProgress: (p) => {
+          setProgress((p.loaded / p.total) * 100);
+        },
       })
-      .catch((error) => {
-        console.log(error);
-      });
+      .then((res) => console.log(res))
+      .catch((res) => console.log(res));
   };
 
   const handlePictureSubmission = (e) => {
-    console.log(albumId, imgUrl);
     e.preventDefault();
     fetch(`https://bazify-backend.basile.vernouillet.dev/api/v1/albums/${albumId}`, {
       method: 'PUT',
-      headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
+      headers: { Authorization: `Bearer ${token}` },
 
       body: JSON.stringify({ picture: imgUrl }),
     })
-      .then((res) => console.log(res))
+      .then((res) => res)
       .catch((err) => err);
   };
 
   return (
-    <div className="w-full h-full flex-col  flex items-center  ">
-      <label htmlFor="">Upload :</label>
+    <div className="w-512 h-full flex-col  flex items-center  ">
+      <div className=" bg-black w-60  h-6 rounded-full">
+        <div style={{ width: `${progress}%` }} className=" h-full bg-white text-center rounded-full"></div>
+      </div>
+      <label htmlFor="">Upload :{progress ? Math.floor(progress) : '00'}% </label>
       <input onChange={handleFile} className="my-2 w-4/5   bg-black bg-opacity-50 rounded-xl" type="file" />
       <input
         onChange={(e) => setImgUrl(e.target.value)}
