e845a168eeab23b0a54e7fff803d4a1657bb3d59
Mathias Ryckewaert <mathryckdel@free.fr>
2021-04-29 16:29:54 +0200

    add the backward arrow on navbar when an artist or album was selected

--- src/components/Carousel/AlbumCarousel/AlbumTrackList.jsx
+++ src/components/Carousel/AlbumCarousel/AlbumTrackList.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Backward from '../../../img/BackwardArrow.svg';
 
-function AlbumTrackList({ handleAlbumClick, item, albumChoice, setSelectedSong }) {
+function AlbumTrackList({ item, albumChoice, setSelectedSong }) {
   const handleClick = (e) => {
     const mySong = item.filter((song) => song.title.includes(e.target.value));
     setSelectedSong(mySong);
   };
   return (
     <div className="h-full w-full">
-      <button onClick={handleAlbumClick}>
-        <img src={Backward} alt="BackwardArrow" />
-      </button>
       <ul className="w-full h-full bg-center bg-no-repeat bg-cover rounded-3xl bg-blend-lighten">
         {item
           .filter((song) => song.album.title.includes(albumChoice))
@@ -30,7 +26,6 @@ function AlbumTrackList({ handleAlbumClick, item, albumChoice, setSelectedSong }
 export default AlbumTrackList;
 
 AlbumTrackList.propTypes = {
-  handleAlbumClick: PropTypes.func.isRequired,
   item: PropTypes.array.isRequired,
   albumChoice: PropTypes.string.isRequired,
   setSelectedSong: PropTypes.func.isRequired,
--- src/components/Carousel/ArtistCarousel/ArtistTrackList.jsx
+++ src/components/Carousel/ArtistCarousel/ArtistTrackList.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Backward from '../../../img/PlayList/ReturnButton.svg';
 
-function ArtistTrackList({ handleArtistClick, item, artistChoice, setSelectedSong }) {
+function ArtistTrackList({ item, artistChoice, setSelectedSong }) {
   const handleClick = (e) => {
     const mySong = item.filter((song) => song.title.includes(e.target.value));
     setSelectedSong(mySong);
   };
   return (
     <div className="flex flex-row-reverse h-full w-full p-4 900:p-8">
-      <button className="focus:outline-none flex justify-start ml-4 mr-2 900:ml-8" onClick={handleArtistClick}>
-        <img src={Backward} alt="BackwardArrow" />
-      </button>
       <ul className=" flex flex-col w-full items-start h-full">
         {item
           .filter((song) => song.artist.name.includes(artistChoice))
@@ -44,7 +40,6 @@ function ArtistTrackList({ handleArtistClick, item, artistChoice, setSelectedSon
 export default ArtistTrackList;
 
 ArtistTrackList.propTypes = {
-  handleArtistClick: PropTypes.func.isRequired,
   item: PropTypes.array.isRequired,
   artistChoice: PropTypes.string.isRequired,
   setSelectedSong: PropTypes.func.isRequired,
