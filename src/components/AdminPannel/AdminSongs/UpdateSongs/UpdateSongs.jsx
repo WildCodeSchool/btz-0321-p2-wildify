c64adf52e62a91fe121661eedae32331cb58cd11
JIdayyy <julienabbadie1@gmail.com>
2021-04-30 09:21:38 +0200

    admin pannel update

--- src/components/AdminPannel/AdminSongs/UpdateSongs/UpdateSongs.jsx
+++ src/components/AdminPannel/AdminSongs/UpdateSongs/UpdateSongs.jsx
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export default function UpdateSongs({ item }) {
+  const [onSelect, setOnSelect] = useState();
+  console.log(onSelect);
   return (
     <div className="flex flex-col  items-center align-middle justify-center w-full">
       <label htmlFor="">Update :</label>
-      <select className="w-72 my-2 h-6 text-white rounded-xl px-4 bg-black bg-opacity-50" name="" id="">
+      <select onChange={(e) => setOnSelect(e.target.value)} className="w-72 my-2 h-6 text-white rounded-xl px-4 bg-black bg-opacity-50" name="" id="">
         {item.map((song, index) => {
-          return <option key={index}>{song.title}</option>;
+          return (
+            <option value={index} key={index}>
+              {song.title}
+            </option>
+          );
         })}
       </select>
-      <input className="w-72 my-2 px-4 h-6 text-white bg-black bg-opacity-50 rounded-xl" type="text" placeholder="Track Title" />
-      <input className="w-72 my-2 px-4 h-6 text-white bg-black bg-opacity-50 rounded-xl" type="text" placeholder="Track Duration" />
-      <input className="w-72 my-2 px-4 h-6 text-white bg-black bg-opacity-50 rounded-xl" type="text" placeholder="Track Picture" />
-      <input className="w-72 my-2 px-4 h-6 text-white bg-black bg-opacity-50 rounded-xl" type="text" placeholder="Track Album" />
-      <input className="w-72 my-2 px-4 h-6 text-white bg-black bg-opacity-50 rounded-xl" type="text" placeholder="Track Artist" />
+      <input
+        className="w-72 my-2 px-4 h-6 text-white bg-black bg-opacity-50 rounded-xl"
+        type="text"
+        placeholder={onSelect ? item[onSelect].title : 'Track Title'}
+      />
+      <input
+        className="w-72 my-2 px-4 h-6 text-white bg-black bg-opacity-50 rounded-xl"
+        type="text"
+        placeholder={onSelect ? item[onSelect].duration : 'Track Title'}
+      />
+      <input
+        className="w-72 my-2 px-4 h-6 text-white bg-black bg-opacity-50 rounded-xl"
+        type="text"
+        placeholder={onSelect ? item[onSelect].album.picture : 'Track Title'}
+      />
+      <input
+        className="w-72 my-2 px-4 h-6 text-white bg-black bg-opacity-50 rounded-xl"
+        type="text"
+        placeholder={onSelect ? item[onSelect].album.title : 'Track Title'}
+      />
+      <input
+        className="w-72 my-2 px-4 h-6 text-white bg-black bg-opacity-50 rounded-xl"
+        type="text"
+        placeholder={onSelect ? item[onSelect].artist.name : 'Track Title'}
+      />
       <button className="border-2 border-white px-4 py-2 rounded-xl focus:outline-none hover:bg-gray-800 active:bg-gray-600">SUBMIT</button>
     </div>
   );
