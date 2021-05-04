3527a3f36e5ab2a31ee3264ccd1c39853a938137
Mathias Ryckewaert <mathryckdel@free.fr>
2021-05-04 10:14:19 +0200

    create the research by artist and album

--- src/components/Carousel/SearchResults/SearchResults.jsx
+++ src/components/Carousel/SearchResults/SearchResults.jsx
@@ -6,11 +6,16 @@ export default function SearchResults({ item, onSearch }) {
     <div className="w-full h-full py-2 pl-2 pr-4 900:px-8">
       <ul>
         {item
-          .filter((song) => song.title.toLowerCase().includes(onSearch))
+          .filter(
+            (song) =>
+              song.title.toLowerCase().includes(onSearch) ||
+              song.artist.name.toLowerCase().includes(onSearch) ||
+              song.album.title.toLowerCase().includes(onSearch),
+          )
           .map((song, index) => {
             return (
-              <li className="font-scada text-white text-sm border-b-2 pb-1 mb-5 hover:text-mainColor hover:border-mainColor" key={index}>
-                {song.title}
+              <li className="font-scada text-white text-sm border-b-2 pb-1 mb-5 hover:text-mainColor hover:border-mainColor flex" key={index}>
+                <p className="mr-4">{song.title}</p>-<p className="mx-4">{song.artist.name}</p>-<p className="mx-4">{song.album.title}</p>
               </li>
             );
           })}
