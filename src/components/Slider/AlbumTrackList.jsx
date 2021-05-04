fb1caba83e4cc32fa330c4c1e84eb52263fbb42b
Thiez-64 <ma.ojeanson@gmail.com>
2021-05-04 20:47:17 +0200

    Add a max width for small phone

--- src/components/Slider/AlbumTrackList.jsx
+++ src/components/Slider/AlbumTrackList.jsx
@@ -32,9 +32,7 @@ export default AlbumTrackList;
 
 AlbumTrackList.propTypes = {
   handleClick: PropTypes.func.isRequired,
-  albums: PropTypes.array.isRequired,
   item: PropTypes.array.isRequired,
   albumChoice: PropTypes.string.isRequired,
-  setCurrentTrack: PropTypes.func.isRequired,
   setSelectedSong: PropTypes.func.isRequired,
 };
